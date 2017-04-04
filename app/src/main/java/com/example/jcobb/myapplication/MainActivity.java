435ac2a62235c789dd6fbdcd063c648327b4c729
John Cobb <john@cphandheld.com>
2017-04-04 12:33:07 -0500

    updates

--- app/src/main/java/com/example/jcobb/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jcobb/myapplication/MainActivity.java
@@ -1,36 +1,18 @@
 package com.example.jcobb.myapplication;
-
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Criteria;
-import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+import com.example.jcobb.myapplication.ConnectivityListener.ConnectivityReceiverListener;
 
-
-//https://github.com/cplain/augmented-reality-tutorial
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ConnectivityReceiverListener {
 
     private static String TAG = "MainActivity";
 
-    private SensorManager senseSensorManager;
-    private Sensor senseSensor;
-    private LocationManager locationLocationManager;
-    private LocationProvider locationLocationProvider;
-
-    Button buttonClickMe;
-    TextView textViewHelloWorld;
-
+    Button buttonCheckConnection;
+    TextView textViewStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,47 +20,38 @@ public class MainActivity extends AppCompatActivity {
         // Tell it to use activity_main as the layout
         setContentView(R.layout.activity_main);
 
-        senseSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senseSensor = (Sensor) senseSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        locationLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationLocationProvider = locationLocationManager.getProvider(locationLocationManager.getBestProvider(Criteria.ACCURACY_FINE, true));
+        buttonCheckConnection = (Button) findViewById(R.id.buttonCheckConnection);
+        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
 
-
-
-        buttonClickMe = (Button) findViewById(R.id.buttonClickMe);
-        textViewHelloWorld = (TextView) findViewById(R.id.textViewHelloWorld);
-
-        buttonClickMe.setOnClickListener(new View.OnClickListener() {
+        buttonCheckConnection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewHelloWorld.setText(R.string.change_text_to);
-
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                checkConnection();
             }
         });
+        checkConnection();
     }
 
-    private final SensorEventListener mListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            Log.d(TAG, "sensorChanged (" + event.values[0] + ", " + event.values[1] );
+    private void checkConnection() {
+        boolean connected = ConnectivityListener.isConnected();
+        showStatus(connected);
+    }
 
-        }
+    private void showStatus(boolean connected) {
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
+        textViewStatus.setText("connected (" + connected + ")");
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        senseSensorManager.registerlistener(null, senseSensor, SensorManager.SENSOR_DELAY_GAME);
-
-
-
+        MyApplication.getInstance().setConnectivityListener(this);
     }
 
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        Log.d(TAG, "connected (" + isConnected + ")");
+        showStatus(isConnected);
+    }
 
 }
